7bf75427a6aa99741f73e19d44c3b1c141713940
danrubel <dan.rubel.usa@gmail.com>
2024-07-08 17:18:25 -0400

    refactor FindDirectoryContaining

--- LatticeRobotCS/PathUtil.cs
+++ LatticeRobotCS/PathUtil.cs
@@ -10,14 +10,23 @@ class PathUtil {
     /// <returns>The path to the directory or `null` if not found.</returns>
     /// <exception cref="Exception"></exception>
     public static string FindDirectoryContaining(string relPath) {
-        // Get the path of the program being executed
         string programPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-        string? dirPath = Path.GetDirectoryName(programPath);
+        return FindDirectoryContaining(relPath, Path.GetDirectoryName(programPath));
+    }
 
-        // Find the Samples directory
+    /// <summary>
+    /// Starting from the specified starting location,
+    /// search to the root directory for a directory containing the specified relative path.
+    /// </summary>
+    /// <param name="relPath">The relative path to be matched</param>
+    /// <param name="startingDirPath">The starting directory path</param>
+    /// <returns>The path to the directory or `null` if not found.</returns>
+    /// <exception cref="Exception"></exception>
+    private static string FindDirectoryContaining(string relPath, string? startingDirPath) {
+        var dirPath = startingDirPath;
         do {
             if (dirPath == null)
-                throw new Exception($"Could not find directory containing {relPath} starting at {programPath}");
+                throw new Exception($"Could not find directory containing {relPath} starting at {startingDirPath}");
             string absPath = Path.Combine(dirPath, relPath);
             if (File.Exists(absPath) || Directory.Exists(absPath)) return dirPath;
             dirPath = Path.GetDirectoryName(dirPath);
